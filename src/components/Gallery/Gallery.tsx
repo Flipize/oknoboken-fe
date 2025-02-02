0f198021940a18de38c56e76c54cf65938f0d48c
Flipize <nilsson.filip@hotmail.se>
2025-02-02 18:26:09 +0100

    Remove unsused imports

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Gallery.css";
 import "../../properties";
 import { properties } from "../../properties";
