3f76522126536fa7fbb01096fd943ceb2af8c612
kalinchukkristina <89469456+kalinchukkristina@users.noreply.github.com>
2025-06-12 23:04:55 +0300

    Scroll to loaded component on nav responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import Menu from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/Banner";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Home from "./components/Home/Home";
 import ContactMe from "./components/ContactMe/ContactMe";
@@ -8,12 +13,32 @@ import Gallery from "./components/Gallery/Gallery";
 import "./App.css";
 import NotFound from "./components/NotFound/NotFound";
 import Order from "./components/Order/Order";
+import { useEffect } from "react";
+
+const ScrollDownToComponent = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const isMobile = window.innerWidth < 768;
+    const target = document.getElementById("banner-end");
+
+    if (isMobile && target) {
+      setTimeout(() => {
+        target.scrollIntoView({ behavior: "smooth", block: "start" });
+      }, 100);
+    }
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
       <Banner />
+      <div id="banner-end" />
       <Menu />
+      <ScrollDownToComponent />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="gallery" element={<Gallery />} />
