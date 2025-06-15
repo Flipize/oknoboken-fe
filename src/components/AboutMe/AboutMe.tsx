8f1f0202ca337fe51e1c1c9d0d4bbfcef6104dcc
Flipize <nilsson.filip@hotmail.se>
2025-06-15 19:04:17 +0200

    Layout adjusted on main page and about me page

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -4,81 +4,92 @@ import Lizette from "../../assets/Lizette.png";
 
 const AboutMe = () => {
   return (
-    <div>
+    <div className="max-w-600 mx-auto">
       <Card title="Om mig">
-        <div className="flex justify-center md:block">
-          <img
-            src={Lizette}
-            alt="Jag"
-            className="w-60 max-w-full mb-3 md:float-left md:mr-6 shadow-[0_8px_30px_rgba(0,0,0,0.3)] border border-gray-200 transform rotate-[0.3deg] hover:scale-105 transition duration-300"
-          />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:mx-30">
+          <div className="space-y-4">
+            <Paragraph>
+              Hej! Jag heter Lizette Nilsson och är författare till boken
+              Oknöboken.
+            </Paragraph>
+            <Paragraph>
+              Jag är född 1963 i Vimmerby. Mitt efternamn som ogift var Anemyr.
+              Eftersom Vimmerby inte hade någon gymnasieskola 1979 fick alla som
+              ville gå gymnasiet åka buss till grannkommunen Hultsfred. Jag gick
+              den samhällsvetenskapliga linjen, en klass med 26 tjejer och fyra
+              killar. En av dessa fyra killar blev senare min man, Mikael
+              Nilsson. Vi blev ett par 1981, förlovade oss 1985, gifte oss 1987,
+              fick första barnet 1992, tvillingar 1994. Innan det var dags för
+              giftermål och barn studerade jag till ekonom i Kristianstad och
+              Växjö. Jag startade min yrkeskarriär på Skattemyndigheten i
+              Vimmerby 1985. År 1988 började jag arbeta som ekonomichef på
+              Roshamns (bytte senare namn till Svelux, Luxo och Glamox) i
+              Målilla. Där arbetade jag i 17 år varav tio av dessa på distans
+              från Huskvarna dit vi flyttade 1995.
+            </Paragraph>
+            <Paragraph>
+              I Jönköping har jag arbetat som ekonomichef dels på ett företag
+              som heter Hettich och min senaste anställning var på Jönköpings
+              kommunala bostadsbolag Vätterhem.
+            </Paragraph>
+            <Paragraph>
+              Under coronaåret 2020 slutade jag på Vätterhem efter nio år. Min
+              man hade några år tidigare fått diagnosen atypisk Parkinson, Lewy
+              Body Demens, vilket är en obotlig sjukdom. Vi beslutade oss för
+              att sälja vårt hus i Huskvarna och flytta till vårt sommarhus på
+              Oknö. Vi ville ta vara på tiden och njuta av varandra och vår
+              paradisö, vi visste att Mikaels hälsotillstånd så småningom skulle
+              bli sämre och sämre. Jag sa upp mig från mitt arbete på Vätterhem
+              och Mikael hade redan slutat sitt arbete några år tidigare. Under
+              tre år som ”lyxlediga” kunde vi njuta av vårt Oknö. Vi skaffade en
+              hundvalp, vilket var MYCKET svårare än jag hade räknat med, men
+              Nelson är nu en härlig ”kompis”.
+            </Paragraph>
+            <Paragraph>
+              Den 21 maj 2023 hände det som inte skulle hända, jag hittar min
+              man medvetslös på badrumsgolvet när jag kommer hem från
+              morgonrundan med Nelson. Jag ringer 112 och jag påbörjar HLR.
+              Ambulansen och Räddningstjänsten kommer snabbt men Mikaels liv går
+              inte att rädda. Jag har förlorat min livskamrat sedan 42 år!
+              Mikael dog inte av sin sjukdom, han hade fått proppar i lungorna,
+              lungemboli. Troligtvis gick allt mycket fort.
+            </Paragraph>
+            <Paragraph>
+              Efter att detta hände är jag så otroligt tacksam för att jag
+              vågade säga upp mig från mitt arbete och flytta till Oknö med
+              Mikael. Vi fick tre år tillsammans som permanentboende på Oknö.
+            </Paragraph>
+            <Paragraph>
+              Man vet inte vad som händer i morgon så därför är min livsfilosofi
+              att ta vara på livet medan man lever. Jag vill uppleva mycket och
+              träffa många människor och göra bra saker, inte bara för mig själv
+              utan även för andra. Jag har tagit uppdrag som god man eftersom
+              jag då kan hjälpa några med mina kunskaper inom bland annat
+              ekonomi.
+            </Paragraph>
+            <Paragraph>
+              Strax efter att min man gick bort började jag med mitt bokprojekt
+              om Oknö. En form av terapi i sorgearbetet men också fantastiskt
+              kul. Jag har lärt känna så många trevliga människor i samband med
+              hundpromenader och intervjuer.
+            </Paragraph>
+            <Paragraph>
+              Mitt engagemang i Oknö Stugförening, där jag är kassör, har också
+              bidragit till att min kärlek till Oknö bara blir större och
+              större.
+            </Paragraph>
+            <Paragraph>
+              Skjut inte upp det du vill göra, lev livet idag!
+            </Paragraph>
+          </div>
+          <div className="flex justify-center md:justify-end">
+            <img
+              src={Lizette}
+              alt="Jag"
+              className="max-h-200 shadow-[0_8px_30px_rgba(0,0,0,0.3)] border border-gray-200"
+            />
+          </div>
         </div>
-        <Paragraph>
-          Hej! Jag heter Lizette Nilsson och är författare till boken Oknöboken.
-        </Paragraph>
-        <Paragraph>
-          Jag är född 1963 i Vimmerby. Mitt efternamn som ogift var Anemyr.
-          Eftersom Vimmerby inte hade någon gymnasieskola 1979 fick alla som
-          ville gå gymnasiet åka buss till grannkommunen Hultsfred. Jag gick den
-          samhällsvetenskapliga linjen, en klass med 26 tjejer och fyra killar.
-          En av dessa fyra killar blev senare min man, Mikael Nilsson. Vi blev
-          ett par 1981, förlovade oss 1985, gifte oss 1987, fick första barnet
-          1992, tvillingar 1994. Innan det var dags för giftermål och barn
-          studerade jag till ekonom i Kristianstad och Växjö. Jag startade min
-          yrkeskarriär på Skattemyndigheten i Vimmerby 1985. År 1988 började jag
-          arbeta som ekonomichef på Roshamns (bytte senare namn till Svelux,
-          Luxo och Glamox) i Målilla. Där arbetade jag i 17 år varav tio av
-          dessa på distans från Huskvarna dit vi flyttade 1995.
-        </Paragraph>
-        <Paragraph>
-          I Jönköping har jag arbetat som ekonomichef dels på ett företag som
-          heter Hettich och min senaste anställning var på Jönköpings kommunala
-          bostadsbolag Vätterhem.
-        </Paragraph>
-        <Paragraph>
-          Under coronaåret 2020 slutade jag på Vätterhem efter nio år. Min man
-          hade några år tidigare fått diagnosen atypisk Parkinson, Lewy Body
-          Demens, vilket är en obotlig sjukdom. Vi beslutade oss för att sälja
-          vårt hus i Huskvarna och flytta till vårt sommarhus på Oknö. Vi ville
-          ta vara på tiden och njuta av varandra och vår paradisö, vi visste att
-          Mikaels hälsotillstånd så småningom skulle bli sämre och sämre. Jag sa
-          upp mig från mitt arbete på Vätterhem och Mikael hade redan slutat
-          sitt arbete några år tidigare. Under tre år som ”lyxlediga” kunde vi
-          njuta av vårt Oknö. Vi skaffade en hundvalp, vilket var MYCKET svårare
-          än jag hade räknat med, men Nelson är nu en härlig ”kompis”.
-        </Paragraph>
-        <Paragraph>
-          Den 21 maj 2023 hände det som inte skulle hända, jag hittar min man
-          medvetslös på badrumsgolvet när jag kommer hem från morgonrundan med
-          Nelson. Jag ringer 112 och jag påbörjar HLR. Ambulansen och
-          Räddningstjänsten kommer snabbt men Mikaels liv går inte att rädda.
-          Jag har förlorat min livskamrat sedan 42 år! Mikael dog inte av sin
-          sjukdom, han hade fått proppar i lungorna, lungemboli. Troligtvis gick
-          allt mycket fort.
-        </Paragraph>
-        <Paragraph>
-          Efter att detta hände är jag så otroligt tacksam för att jag vågade
-          säga upp mig från mitt arbete och flytta till Oknö med Mikael. Vi fick
-          tre år tillsammans som permanentboende på Oknö.
-        </Paragraph>
-        <Paragraph>
-          Man vet inte vad som händer i morgon så därför är min livsfilosofi att
-          ta vara på livet medan man lever. Jag vill uppleva mycket och träffa
-          många människor och göra bra saker, inte bara för mig själv utan även
-          för andra. Jag har tagit uppdrag som god man eftersom jag då kan
-          hjälpa några med mina kunskaper inom bland annat ekonomi.
-        </Paragraph>
-        <Paragraph>
-          Strax efter att min man gick bort började jag med mitt bokprojekt om
-          Oknö. En form av terapi i sorgearbetet men också fantastiskt kul. Jag
-          har lärt känna så många trevliga människor i samband med
-          hundpromenader och intervjuer.
-        </Paragraph>
-        <Paragraph>
-          Mitt engagemang i Oknö Stugförening, där jag är kassör, har också
-          bidragit till att min kärlek till Oknö bara blir större och större.
-        </Paragraph>
-        <Paragraph>Skjut inte upp det du vill göra, lev livet idag!</Paragraph>
       </Card>
     </div>
   );
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,12 @@
 import Card from "../Card";
 import Boken from "../../assets/boken.jpg";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Paragraph from "../Paragraph";
 
 const Main = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (location.state?.scrollToOknoboken) {
@@ -17,62 +18,77 @@ const Main = () => {
   }, [location]);
 
   return (
-    <div data-scroll-target="oknoboken" className="scroll-mt-20">
+    <div
+      data-scroll-target="oknoboken"
+      className="scroll-mt-20 max-w-600 mx-auto"
+    >
       <Card title="Oknöboken">
-        <div>
-          <div className="flex justify-center md:block">
-            <img
-              src={Boken}
-              alt="Oknoboken"
-              className="w-60 max-w-full mb-3 md:float-right md:mr-6 shadow-[0_8px_30px_rgba(0,0,0,0.3)] border border-gray-200 transform rotate-[0.3deg] hover:scale-105 transition duration-300"
-            />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:mx-30">
+          <div className="space-y-4">
+            <Paragraph>
+              Min bok om Oknö är naturligtvis en hyllning till denna underbara
+              plats men jag vill också genom boken ge fler möjlighet att få
+              kunskap om öns historia. Det finns många som har eller har haft
+              ett hus på Oknö som inte känner till hur det kom sig att det blev
+              en semesterö.
+            </Paragraph>
+            <Paragraph>
+              Jag har haft förmånen att få bosätta mig här permanent sedan 2020
+              och har haft mitt hus sedan 1993. Då visste inte jag heller mycket
+              om Oknö eller dess historia. Att jag skulle skriva en bok drygt
+              trettio år senare trodde jag inte då!
+            </Paragraph>
+            <Paragraph>
+              Oknö är en ö strax utanför Mönsterås på Ostkusten, mitt emellan
+              Kalmar och Oskarshamn. Ön har broförbindelse med fastlandet och
+              här bor cirka 400 personer permanent och klassificeras som en
+              tätort. Här finns fler än 500 fastigheter, två campingplatser, sex
+              kommunala badplatser, naturförskola och en underbar natur.
+            </Paragraph>
+            <Paragraph>
+              År 1950 öppnade Sveriges första kommunala semesterby som några år
+              senare hade 127 uthyrningsstugor. Platsen blev känd över hela
+              Sverige för dess fina stugor med hög standard med dåtidens mått
+              för en billig peng som alla hade råd med.
+            </Paragraph>
+            <Paragraph>
+              Innan Oknö blev ett semesterparadis brukades jorden av
+              torparfamiljer redan från slutet av 1600-talet.
+            </Paragraph>
+            <Paragraph>
+              I boken kan man läsa om de första tre torpen, de första
+              sommarstugorna och den första kommunala semesterbyn i Sverige. Här
+              finns också berättelser om Karolina, hon som sov i 32 år,
+              lotsstationen, färjeläget, vattenrutschkanan på Lillön och mycket
+              mer.
+            </Paragraph>
+            <Paragraph>
+              Jag hoppas du är intresserad och vill köpa boken. Skicka din
+              adress till min mejladress info@lizettavonsmil.se, dit du vill att
+              jag ska skicka boken, och swisha totalt 325 kr varav 75 kr avser
+              frakt. Vill du hellre hämta den hemma hos mig på Lillövägen 36 på
+              Oknö, Mönsterås, går det också bra, då är priset endast 250 kr.
+              Meddela innan så jag är hemma. Boken kommer också att finnas till
+              försäljning hos Erikas Galleri och Atelje på Storgatan i
+              Mönsterås.
+            </Paragraph>
           </div>
-          <Paragraph>
-            Min bok om Oknö är naturligtvis en hyllning till denna underbara
-            plats men jag vill också genom boken ge fler möjlighet att få
-            kunskap om öns historia. Det finns många som har eller har haft ett
-            hus på Oknö som inte känner till hur det kom sig att det blev en
-            semesterö.
-          </Paragraph>
-          <Paragraph>
-            Jag har haft förmånen att få bosätta mig här permanent sedan 2020
-            och har haft mitt hus sedan 1993. Då visste inte jag heller mycket
-            om Oknö eller dess historia. Att jag skulle skriva en bok drygt
-            trettio år senare trodde jag inte då!
-          </Paragraph>
-          <Paragraph>
-            Oknö är en ö strax utanför Mönsterås på Ostkusten, mitt emellan
-            Kalmar och Oskarshamn. Ön har broförbindelse med fastlandet och här
-            bor cirka 400 personer permanent och klassificeras som en tätort.
-            Här finns fler än 500 fastigheter, två campingplatser, sex kommunala
-            badplatser, naturförskola och en underbar natur.
-          </Paragraph>
-          <Paragraph>
-            År 1950 öppnade Sveriges första kommunala semesterby som några år
-            senare hade 127 uthyrningsstugor. Platsen blev känd över hela
-            Sverige för dess fina stugor med hög standard med dåtidens mått för
-            en billig peng som alla hade råd med.
-          </Paragraph>
-          <Paragraph>
-            Innan Oknö blev ett semesterparadis brukades jorden av
-            torparfamiljer redan från slutet av 1600-talet.
-          </Paragraph>
-          <Paragraph>
-            I boken kan man läsa om de första tre torpen, de första
-            sommarstugorna och den första kommunala semesterbyn i Sverige. Här
-            finns också berättelser om Karolina, hon som sov i 32 år,
-            lotsstationen, färjeläget, vattenrutschkanan på Lillön och mycket
-            mer.
-          </Paragraph>
-          <Paragraph>
-            Jag hoppas du är intresserad och vill köpa boken. Skicka din adress
-            till min mejladress info@lizettavonsmil.se, dit du vill att jag ska
-            skicka boken, och swisha totalt 325 kr varav 75 kr avser frakt. Vill
-            du hellre hämta den hemma hos mig på Lillövägen 36 på Oknö,
-            Mönsterås, går det också bra, då är priset endast 250 kr. Meddela
-            innan så jag är hemma. Boken kommer också att finnas till
-            försäljning hos Erikas Galleri och Atelje på Storgatan i Mönsterås.
-          </Paragraph>
+          <div className="flex justify-center md:justify-end">
+            <div className="flex flex-col items-center md:items-end">
+              <img
+                src={Boken}
+                alt="Oknoboken"
+                className="max-w-100 shadow-[0_8px_30px_rgba(0,0,0,0.3)] border border-gray-200 w-9/10"
+              />
+              <button
+                type="button"
+                className="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition w-9/10"
+                onClick={() => navigate("/order")}
+              >
+                Köp
+              </button>
+            </div>
+          </div>
         </div>
       </Card>
     </div>
